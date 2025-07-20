8ee1408e0100581a4753851e4be7575e1bc1b432
sy-fedora <sy-fedora@192.168.1.8>
2025-07-20 17:49:31 +0530

    ADDED: sample user model

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: null
+    },
+    email: {
+        type: String,
+        default: null,
+    },
+    password: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
