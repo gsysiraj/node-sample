2df8a7c6947d4b4e413dbfae22d27237a267c400
sy-fedora <sy-fedora@192.168.1.8>
2025-07-20 12:21:48 +0530

    ADDED: express

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require("./app")
+
+app.get('/', (req, res) => {
+    console.log("Server is up and running")
+    res.send("Server is up and running")
+})
+
+app.listen('3000', () => {
+    console.log("listening on port 3000")
+})
